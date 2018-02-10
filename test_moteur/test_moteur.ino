5cfdcfe220494c13a3ef935d0c7419ec72426004
Mourad <aumarmourad.pro@gmail.com>
2018-02-10 15:28:24 +0100

    test detecteur ultrason

--- test_moteur/test_moteur.ino
+++ test_moteur/test_moteur.ino
@@ -2,8 +2,8 @@
 #include <TimerOne.h>
 
 //variables pour les interrupeurs moteurs
-const byte motor1 = 2; //interrupteur du moteur1 correspondant à la PIN 2 --> PIN-0 arduiono
-const byte motor2 = 3; //interrupteur du moteur1 correspondant à la PIN 2 --> PIN-1 arduino
+const byte PinMoteur1 = 2; //interrupteur du moteur1 correspondant à la PIN 2 --> PIN-0 arduiono
+const byte PinMoteur2 = 3; //interrupteur du moteur1 correspondant à la PIN 2 --> PIN-1 arduino
 
 //variables pour le compteur d'implusion
 unsigned int compteur1 = 0;
@@ -31,10 +31,30 @@ void ISR_timerone()
 {
   Timer1.detachInterrupt(); // arrete le timer
   Serial.print("Vitesse du moteur 1: ");
-  float rotation1 = (compteur1/diskSlots) * 60.00;
+  float rotation1 = (compteur1/diskSlots) * 60.00;// calcul du nombre de tour/min moteur1
   Serial.print(rotation1);
   Serial.print("tr/min: ");
   compteur1 = 0; //remise à zero du compteur1
+  Serial.print("Vitesse du moteur 2: ");
+  float rotation2 = (compteur2/diskSlots) * 60.00;// calcul du nombre de tour/min moteur1
+  Serial.print(rotation2);
+  Serial.print("tr/min: ");
+  compteur2 = 0; //remise à zero du compteur2
+  Timer1.attachInterrupt(ISR_timerone); // arrete le timer
 }
 
+void setup()
+{
+  Serial.begin(9600);
+  Timer1.initialize(1000000);//reglage du timer à 1sec
+  attachInterrupt(PinMoteur1, ISR_compteur1, RISING); //augmente le compteur1 lorsque le detecteur s'active
+  attachInterrupt(PinMoteur2, ISR_compteur2, RISING); //augmente le compteur2 lorsque le detecteur s'active
+  Timer1.attachInterrupt(ISR_timerone); //active le timer
+  Serial.print("tr/min: ");
+}
+void loop()
+{
+  //aps necessaire
+  
+}
 
